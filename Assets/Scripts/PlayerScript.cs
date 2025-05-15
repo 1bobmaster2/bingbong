116e2b9647e6537f6a20bbb64c2eda2f1c9f762f
1bobmaster2 <fkuba556@gmail.com>
2025-05-15 19:01:19 +0200

    ok bro i swear

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,16 +8,31 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public Vector3 worldPosition;
     
     // Update is called once per frame
+    void Start()
+    {
+        GameObject camObj = GameObject.FindWithTag("Player1Cam");
+        cam = camObj.GetComponent<Camera>();
+        if (cam == null)
+        {
+            Debug.LogError("No camera found with the tag 'Player1Cam'");
+        }
+    }
     void Update()
     {
         // TODO: return if not owner, only do after adding multiplayer!!!!!
         
         screenPosition = Input.mousePosition;
-        screenPosition.z = cam.nearClipPlane + 5;
+        /*screenPosition.z = cam.nearClipPlane + 2f;
         
-        worldPosition = cam.ScreenToWorldPoint(screenPosition);
+        worldPosition = cam.ScreenToWorldPoint(screenPosition);*/
+        
+        Ray ray = cam.ScreenPointToRay(screenPosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitData))
+        {
+            worldPosition = hitData.point;
+        }
         
         transform.position = worldPosition;
     }
-   
 }
