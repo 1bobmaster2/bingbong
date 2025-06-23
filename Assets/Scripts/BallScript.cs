966e7b711be630d3c28183c1a24f5d2087da0825
1bobmaster2 <fkuba556@gmail.com>
2025-06-23 19:42:43 +0200

    a

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    [SerializeField] private float force;
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject hit = collision.gameObject;
+
+
+        ContactPoint contact = collision.contacts[0];
+
+        Vector3 incomingVelocity = GetComponent<Rigidbody>().linearVelocity;
+
+        
+        Vector3 reflectedVelocity = Vector3.Reflect(incomingVelocity, contact.normal);
+
+        
+        Vector3 forceToAdd = reflectedVelocity.normalized * incomingVelocity.magnitude * force;
+        
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.linearVelocity = Vector3.zero;
+
+        
+        rb.AddForce(forceToAdd, ForceMode.VelocityChange);
+        
+        if (hit.CompareTag("HostPlayer") || hit.CompareTag("ClientPlayer"))
+        {
+
+        }
+    }
+}
