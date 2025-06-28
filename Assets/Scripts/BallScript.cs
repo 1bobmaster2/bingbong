8e3246b86e62380e7442c4cbf7412879bd516af5
1bobmaster2 <fkuba556@gmail.com>
2025-06-28 21:40:05 +0200

    changed mono to network

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,7 +1,7 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class BallScript : MonoBehaviour
+public class BallScript : NetworkBehaviour
 {
     [SerializeField] private float force;
     [Space]
