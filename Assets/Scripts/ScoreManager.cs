20e457c813e7fe5ec951dd446d669bb5a6c61b38
1bobmaster2 <fkuba556@gmail.com>
2025-08-06 15:07:00 +0200

    added rpc for adding score

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -7,5 +7,22 @@ public class ScoreManager : NetworkBehaviour
     public static NetworkVariable<int> HostScore  = new NetworkVariable<int>(); 
     public static NetworkVariable<int> ClientScore  = new NetworkVariable<int>(); 
     void Awake() => instance = this;
-    
+
+
+    [ServerRpc(RequireOwnership = false)]
+    public void AddScoreServerRpc(ServerRpcParams p = default)
+    {
+        ulong senderId = p.Receive.SenderClientId;
+
+        if (senderId == 0)
+        {
+            HostScore.Value++;
+            Debug.Log("Host got 1 score, host score is now: " + HostScore.Value);
+        }
+        else
+        {
+            ClientScore.Value++;
+            Debug.Log("Client got 1 score, client score is now: " + ClientScore.Value);
+        }
+    }
 }
