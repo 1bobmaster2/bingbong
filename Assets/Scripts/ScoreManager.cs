9a6d673f4498a241cabdf095270bf08e61203de5
1bobmaster2 <fkuba556@gmail.com>
2025-08-06 15:06:52 +0200

    added network variables

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public class ScoreManager : NetworkBehaviour
 {
     public static ScoreManager instance;
-    
+    public static NetworkVariable<int> HostScore  = new NetworkVariable<int>(); 
+    public static NetworkVariable<int> ClientScore  = new NetworkVariable<int>(); 
     void Awake() => instance = this;
     
 }
