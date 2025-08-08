635b5eb5e9d7f2f2e7d5bf8523603a38f05c1d7e
1bobmaster2 <fkuba556@gmail.com>
2025-08-08 12:13:07 +0200

    idk

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,7 +5,7 @@ public class ScoreManager : NetworkBehaviour
 {
     public static ScoreManager instance;
     public static NetworkVariable<int> HostScore  = new NetworkVariable<int>(); 
-    public static NetworkVariable<int> ClientScore  = new NetworkVariable<int>(); 
+    public static NetworkVariable<int> ClientScore  = new NetworkVariable<int>();
     void Awake() => instance = this;
 
 
