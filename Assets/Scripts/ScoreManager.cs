20b22cbdea37f407dce1cc291f2d78781a868fe6
1bobmaster2 <fkuba556@gmail.com>
2025-08-10 17:07:53 +0200

    subscribtions

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -10,6 +10,19 @@ public class ScoreManager : NetworkBehaviour
     public static NetworkVariable<int> ClientScore  = new NetworkVariable<int>();
     void Awake() => instance = this;
 
+    public override void OnNetworkSpawn()
+    {
+        HostScore.OnValueChanged += (oldValue, newValue) =>
+        {
+            UpdateTextOnAllClientsServerRpc();
+        };
+        ClientScore.OnValueChanged += (oldValue, newValue) =>
+        {
+            UpdateTextOnAllClientsServerRpc();
+        };
+    }
+    
+    
 
     [ServerRpc(RequireOwnership = false)]
     public void AddScoreServerRpc(string winner, ServerRpcParams p = default)
