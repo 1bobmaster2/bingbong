369d3e7eac693b9d6e8304be0d7661347128eafc
1bobmaster2 <fkuba556@gmail.com>
2025-08-24 08:30:56 +0200

    i just noticed, fixed

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
-public class NewMonoBehaviourScript : NetworkBehaviour
+public class PlayerScript : NetworkBehaviour
 {
     [SerializeField] private float zoomFactor;
     [Space]
