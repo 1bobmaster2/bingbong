8bb34fdf0f460a643c91daddae9d1ae4d82f2c85
1bobmaster2 <fkuba556@gmail.com>
2025-08-24 08:32:05 +0200

    logic for setReady

--- Assets/Scripts/MatchReadyManager.cs
+++ Assets/Scripts/MatchReadyManager.cs
@@ -6,6 +6,11 @@ public class MatchReadyManager : MonoBehaviour
 
     public void SetReady()
     {
+        GameObject caller = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
+        Destroy(caller);
         
+        GameObject hostPlayer = GameObject.FindWithTag("HostPlayer");
+        PlayerScript playerScript = hostPlayer.GetComponent<PlayerScript>();
+        playerScript.SpawnNetworkObject(ballPrefab);
     }
 }
