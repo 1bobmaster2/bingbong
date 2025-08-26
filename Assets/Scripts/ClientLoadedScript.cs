b23fe1b512971ed3b3b50959645e752a58f50c42
1bobmaster2 <fkuba556@gmail.com>
2025-08-26 08:26:29 +0200

    Renamed ClientReadyScript to ClientLoadedScript

---
+++ Assets/Scripts/ClientLoadedScript.cs
@@ -0,0 +1,18 @@
+using Unity.Netcode;
+
+public class ClientLoadedScript : NetworkBehaviour
+{
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) return;
+        
+        NotifyHandlerOnReadyServerRpc(); // notifies the host when the client has joined
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void NotifyHandlerOnReadyServerRpc(ServerRpcParams p = default)
+    {
+        ulong clientId = p.Receive.SenderClientId;
+        ClientReadyHandler.instance.MarkClientReady(clientId); // marks the client ready
+    }
+}
