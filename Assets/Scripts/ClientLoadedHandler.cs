b262d7d38634d618841a475a5a9b7959628af8d1
1bobmaster2 <fkuba556@gmail.com>
2025-08-26 08:26:13 +0200

    renamed clientReadyHandler to clientLoadedHandler

---
+++ Assets/Scripts/ClientLoadedHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientLoadedHandler : MonoBehaviour
+{
+    public static ClientLoadedHandler instance;
+    private HashSet<ulong> readyClients = new HashSet<ulong>();
+    [SerializeField] public int expectedPlayers;
+    public bool allClientsLoaded;
+    
+    void Awake() => instance = this;
+
+    public void MarkClientReady(ulong clientId)
+    {
+        readyClients.Add(clientId);
+
+        if (readyClients.Count == expectedPlayers) // if the number of joined clients is equal to the expected players, we set allClientsReady to true
+        {
+            allClientsLoaded = true;
+            Debug.Log("All clients ready");
+        }
+    }
+}
