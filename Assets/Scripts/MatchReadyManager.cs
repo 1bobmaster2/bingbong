fbb1ae253ff98d6135f7873227c0ca162885df9e
1bobmaster2 <fkuba556@gmail.com>
2025-08-27 08:22:23 +0200

    inherits from networkbehaviour

--- Assets/Scripts/MatchReadyManager.cs
+++ Assets/Scripts/MatchReadyManager.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class MatchReadyManager : MonoBehaviour
+public class MatchReadyManager : NetworkBehaviour
 {
     [SerializeField] private GameObject ballPrefab;
 
