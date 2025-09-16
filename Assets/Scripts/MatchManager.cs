7e82a1cfb81aa2e2a2a5fef2694776f4fae8bef5
1bobmaster2 <fkuba556@gmail.com>
2025-09-16 12:41:00 +0200

    Finished one TODO, what a W

--- Assets/Scripts/MatchManager.cs
+++ Assets/Scripts/MatchManager.cs
@@ -16,7 +16,6 @@ public class MatchManager : NetworkBehaviour
         //startRoundButtonObject.SetActive(true); // we set the button active when the host and client load in
         //coinFlipButtonObject.SetActive(true);
         
-        //TODO: spawn the buttons
         //TODO: set the onclick listeners
         if (IsServer) // we only execute this on the server as only the server should handle spawning objects
         {
