92c42f6ac98127f97f69af4a5ce246f54575a62a
1bobmaster2 <fkuba556@gmail.com>
2025-10-07 13:10:35 +0200

    functionality

--- Assets/Scripts/PlayerSoundManager.cs
+++ Assets/Scripts/PlayerSoundManager.cs
@@ -8,11 +8,15 @@ public class PlayerSoundManager : NetworkBehaviour
         {
             if (IsHost)
             {
-                
+                GameObject otherPlayer = GameObject.FindGameObjectWithTag("ClientPlayer");
+                AudioListener otherPlayerListener = otherPlayer.GetComponentInChildren<AudioListener>();
+                otherPlayerListener.enabled = false;
             }
             else if (IsClient)
             {
-                
+                GameObject otherPlayer = GameObject.FindGameObjectWithTag("HostPlayer");
+                AudioListener otherPlayerListener = otherPlayer.GetComponentInChildren<AudioListener>();
+                otherPlayerListener.enabled = false;
             }
         }
     }
