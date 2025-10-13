51a9ff5e7080f0655b95efc96f099514687e4934
1bobmaster2 <fkuba556@gmail.com>
2025-10-13 12:36:19 +0200

    added variable

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -19,6 +19,8 @@ public class BallScript : NetworkBehaviour
 
     private int playerServing; // 1 means host 2 means client
     private int playerServingSwitchHelperVar; // helper variable for switching the serving player
+
+    private int amountOfBounces;
     
     void Start()
     {
