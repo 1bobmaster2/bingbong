cb89a373d7a8bfd9d32b5e1fcca34a0ead512e97
1bobmaster2 <fkuba556@gmail.com>
2025-10-17 15:59:12 +0200

    usage of vec

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -201,7 +201,8 @@ public class BallScript : NetworkBehaviour
                 }
                 else if (playerServing == 2)
                 {
-                    MoveBallToSpawn(new Vector3(0.7f, 0, 7.6f));
+                    vec = new Vector3(0.7f, 0, 7.6f);
+                    MoveBallToSpawn(vec);
                     Debug.Log($"moved ball to {vec.x}, {vec.y}, {vec.z}");
                     return;
                 }
