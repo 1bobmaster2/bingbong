0cad2d3bef165f5a83d44c948788aa8057fe93cb
1bobmaster2 <fkuba556@gmail.com>
2025-11-03 16:32:37 +0100

    new

--- Assets/Scripts/ScoreCheck.cs
+++ Assets/Scripts/ScoreCheck.cs
@@ -103,5 +103,7 @@ public class ScoreCheck : NetworkBehaviour
                 }
             }
         }
+        
+        SnapshotForGroundAmountOfBounces = ballScript.amountOfBounces;
     }
 }
