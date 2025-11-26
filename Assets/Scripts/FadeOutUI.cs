3e81ec55d2ae15c5c7115299920d6091169b832e
1bobmaster2 <fkuba556@gmail.com>
2025-11-26 17:44:51 +0100

    setting the text var

--- Assets/Scripts/FadeOutUI.cs
+++ Assets/Scripts/FadeOutUI.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 public class FadeOutUI : MonoBehaviour
 {
-    
     private TextMeshProUGUI text;
+
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
 }
