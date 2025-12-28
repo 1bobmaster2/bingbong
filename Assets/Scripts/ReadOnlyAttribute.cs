3881202fad82027f2c699df60d9564d64810ebf9
1bobmaster2 <fkuba556@gmail.com>
2025-12-28 10:16:40 +0100

    ok

--- Assets/Scripts/ReadOnlyAttribute.cs
+++ Assets/Scripts/ReadOnlyAttribute.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ReadOnlyAttribute : PropertyAttribute
+public class ReadOnlyAttribute : PropertyAttribute //
 {
     // this attribute makes a variable read only from the unity inspector
 }
