34da4ab9f31a2c8c719d98b650de68fed212b86a
1bobmaster2 <fkuba556@gmail.com>
2026-01-13 12:36:21 +0100

    if

--- Assets/Scripts/PlayerSoundManager.cs
+++ Assets/Scripts/PlayerSoundManager.cs
@@ -7,12 +7,10 @@ public class PlayerSoundManager : NetworkBehaviour
     
     private void Update()
     {
-        if (ClientLoadedHandler.instance.allClientsLoaded)
-        {
-            if (!IsOwner) return;//
-            
-            
         if (!IsOwner) return;//
+
+        if (!done)
+        {
             if (IsHost) // this script disables the audio listener of the other player, also sometimes doesnt work (doesnt work if the host is hosting from an instance not from the unity editor)
             {
                 
