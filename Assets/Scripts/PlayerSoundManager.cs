07a45e2aae63998db09e2f1f7cef39001b0e28dc
1bobmaster2 <fkuba556@gmail.com>
2026-01-13 12:36:57 +0100

    added done setting

--- Assets/Scripts/PlayerSoundManager.cs
+++ Assets/Scripts/PlayerSoundManager.cs
@@ -45,6 +45,7 @@ public class PlayerSoundManager : NetworkBehaviour
                     return;
                 }
                 otherPlayerListener.enabled = false;
+                done = true;
             }
         }
     }
