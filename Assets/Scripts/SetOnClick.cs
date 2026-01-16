e58b9f09f59ac34f750fddbf535f360b81f7428c
1bobmaster2 <fkuba556@gmail.com>
2026-01-16 13:50:51 +0100

    doenst work

--- Assets/Scripts/SetOnClick.cs
+++ Assets/Scripts/SetOnClick.cs
@@ -1,6 +1,24 @@
+using System;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetOnClick : NetworkBehaviour
 {
-    
+    [SerializeField] private Button button;
+    private void Update()
+    {
+        if (IsHost)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("HostPlayer");
+            PlayerScript playerScript = player.GetComponent<PlayerScript>();
+            button.onClick.AddListener(playerScript.LeaveGame);
+        }
+        else // when client
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("ClientPlayer");
+            PlayerScript playerScript = player.GetComponent<PlayerScript>();
+            button.onClick.AddListener(playerScript.LeaveGame);
+        }
+    }
 }
